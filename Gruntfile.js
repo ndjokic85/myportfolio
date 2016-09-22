68353ecdc85a9dbe690668afbb39eb2391a46ba9
Nikola <ekv1sky@gmail.com>
2016-09-22 21:14:51 +0200

    Add mincss.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,22 @@ module.exports = function (grunt) {
     // 1. All configuration goes here 
     grunt.initConfig({
         concat: {
-            dist: {
+            js: {
                 src: [
                     'public/js/libs/*.js', // All JS in the libs folder
 
                 ],
                 dest: 'public/js/script.js',
-            }
+            },
+            css:
+                    {
+                        src:[
+                       'public/css/*.css',
+                       
+                        ],
+                        dest: 'public/css/style.min.css'
+                    }
+           
         },
         watch:
                 {
@@ -17,7 +26,14 @@ module.exports = function (grunt) {
                             {
                                 files: ['public/js/libs/*.js'],
                                 tasks: ['uglify'],
-                            }
+                            },
+                    css:
+                    {
+                        files: ['public/css/*.css'],
+                        tasks: ['cssmin'],
+                    }
+                            
+             
                 },
         uglify: {
             build: {
@@ -25,15 +41,30 @@ module.exports = function (grunt) {
                 dest: 'public/js/script-min.js',
             }
         },
+        cssmin:
+        {
+            my_target:
+            {
+                files:[
+            {
+                expand:true,
+                src:['public/css/*.css'],
+                dest:'',
+                ext:'.min.css'
+            }
+                ]
+            }
+        }
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
 
     // 4. Where we tell Grunt what to do when we type 'grunt' into the terminal.
-    grunt.registerTask('default', ['uglify', 'concat', 'watch']);
+    grunt.registerTask('default', ['uglify', 'concat', 'watch','cssmin']);
 
 };
